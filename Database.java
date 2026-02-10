d3fdbd7294b1ab5a7ea3219f4da410563bc6bdd4
GHarr3 <jacob.harr479@gmail.com>
2026-02-10 13:30:39 -0600

    Added methods to the Database class Database.addPlayer(int id, str name) adds a player to the table with the provided information.
    Database.removePlayer has 3 variations which remove players with characteristics matching the information provided.
    Database.removePlayer(int id) removes any player with the given id
    Database.removePlayer(str name) removes any player with the given codename
    Database.removePlayer(int id, str name) removes any player with the given id and name combination
    Both the name and id need to be a match for the third variant to work, it is an AND not an OR
    
    Database.printPlayers() prints the players table from the photon database

--- Database.java
+++ Database.java
@@ -28,11 +28,62 @@ public class Database {
         //establish connection to the database
         try {
             this.connection = DriverManager.getConnection(URL, "student", "student");
-            System.out.println("Connection successful!");
+            System.out.println("Connection to the database was successful!");
         }
         catch(SQLException e) {
             System.out.println("Could not connect to the database!");
             System.out.println(e.getMessage());
         }
+    }
+
+    //  ADD AND REMOVE
+    //addPlayer method
+    //returns true when a player is successfully added to the database, false otherwise
+    public boolean addPlayer(int id, String name) {    
+        try(PreparedStatement pstmt = connection.prepareStatement("INSERT INTO players2 (id, codename) VALUES (?, ?)")) {
+            pstmt.setInt(1, id);
+            pstmt.setString(2, name);
+            pstmt.executeUpdate();
+            
+            return true;
+        }
+
+        catch(SQLException e) {
+            System.err.println("SQL exception occurred whille attempting to add a user: " + e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    //printPlayers method
+    public void printPlayers() {
+        //fetching data from table
+        try(Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM players2")) {
+
+            //getting column data
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int colsCount = rsmd.getColumnCount();
+            
+            //printing data
+            System.out.println("\n\n");
+            System.out.println("Player list:");
+            System.out.printf("%-10s", "ID:");
+            System.out.printf("%-10s", "Codename:");
+            System.out.println();
+
+            
+            while(rs.next()) {
+                for (int i = 1; i <= colsCount; i++) {
+                    System.out.printf("%-10s", rs.getString(i));
+                }
+                System.out.println();
+            }
+        }
+        catch(SQLException e) {
+            System.err.println("SQL exception occurred whille attempting to print users: " + e.getMessage());
+            e.printStackTrace();
+        }
+        
     }
 }
--- test.java
+++ test.java
@@ -2,5 +2,6 @@ public class test {
 
     public static void main(String[] args) {
         Database data = new Database();
+        data.printPlayers();
     }
 }
