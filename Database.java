f60bd87c322cda2412182373013993714dc3d88e
GHarr3 <jacob.harr479@gmail.com>
2026-02-10 13:58:31 -0600

    Fixed a small typo in Database class Updated test to add 2 players for the purpose of the sprint

--- Database.java
+++ Database.java
@@ -21,7 +21,7 @@ public class Database {
 
     //  VARS
     private static final String URL = "jdbc:postgresql:photon";
-    private static Connection connection;
+    private Connection connection;
 
     //  CONSTRUCTOR
     public Database() {
@@ -38,9 +38,11 @@ public class Database {
 
     //  ADD AND REMOVE
     //addPlayer method
+    //params: id and name
+    //creates a new player in the database with ID id and codename name.
     //returns true when a player is successfully added to the database, false otherwise
     public boolean addPlayer(int id, String name) {    
-        try(PreparedStatement pstmt = connection.prepareStatement("INSERT INTO players2 (id, codename) VALUES (?, ?)")) {
+        try(PreparedStatement pstmt = this.connection.prepareStatement("INSERT INTO players (id, codename) VALUES (?, ?)")) {
             pstmt.setInt(1, id);
             pstmt.setString(2, name);
             pstmt.executeUpdate();
@@ -55,11 +57,88 @@ public class Database {
         }
     }
 
+    //removePlayers methods
+    //ID version
+    //params: id
+    //removes players from the database with the given id
+    //returns the number of players removed from the database
+    public int removePlayers(int id) {
+        int updated = 0;
+
+        //attempting to remove all users with matching id
+        try(PreparedStatement pstmt = this.connection.prepareStatement("DELETE FROM players WHERE id = ?")) {
+            pstmt.setInt(1, id);
+            updated = pstmt.executeUpdate();
+        }
+
+        catch(SQLException e) {
+            System.err.println("SQL exception occurred whille attempting to remove users: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        //printing and returning results
+        System.out.println();
+        System.out.println(updated == 1 ? "An entry was removed from the table." : updated + " entries were removed from the table.");
+        return updated;
+    }
+
+    //Codename version
+    //params: name
+    //removes players from the database with the given codename
+    //returns the number of players removed from the database
+    public int removePlayers(String name) {
+        int updated = 0;
+
+        //attempting to remove all users with matching codename
+        try(PreparedStatement pstmt = this.connection.prepareStatement("DELETE FROM players WHERE codename = ?")) {
+            pstmt.setString(1, name);
+            updated = pstmt.executeUpdate();
+        }
+
+        catch(SQLException e) {
+            System.err.println("SQL exception occurred whille attempting to remove users: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        //printing and returning results
+        System.out.println();
+        System.out.println(updated == 1 ? "An entry was removed from the table." : updated + " entries were removed from the table");
+        return updated;
+    }
+
+    //full version
+    //params: id and codename 
+    //removes players from the database with the given id and codename combination
+    //returns the number of players removed from the database
+    public int removePlayers(int id, String name) {
+        int updated = 0;
+
+        //attempting to remove all users with matching id and codename
+        try(PreparedStatement pstmt = this.connection.prepareStatement("DELETE FROM players WHERE id = ? AND codename = ?")) {
+            pstmt.setInt(1, id);
+            pstmt.setString(2, name);
+            updated = pstmt.executeUpdate();
+        }
+
+        catch(SQLException e) {
+            System.err.println("SQL exception occurred whille attempting to remove users: " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        //printing and returning results
+        System.out.println();
+        System.out.println(updated == 1 ? "An entry was removed from the table." : updated + " entries were removed from the table");
+        return updated;
+    }
+
+
+
+
     //printPlayers method
     public void printPlayers() {
         //fetching data from table
         try(Statement stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM players2")) {
+            ResultSet rs = stmt.executeQuery("SELECT * FROM players")) {
 
             //getting column data
             ResultSetMetaData rsmd = rs.getMetaData();
--- test.java
+++ test.java
@@ -2,6 +2,11 @@ public class test {
 
     public static void main(String[] args) {
         Database data = new Database();
+        data.removePlayers(2);
+        data.removePlayers(3);
+        data.printPlayers();
+        data.addPlayer(2, "Alpha");
+        data.addPlayer(3, "Bravo");
         data.printPlayers();
     }
 }
